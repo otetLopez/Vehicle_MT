714f351b6ce8ef1d6bb2404132b534901698e926
otetLopez <otet.tud@gmail.com>
2019-12-05 14:48:49 -0500

    Vehicle MT: Corrected Some layout changes

--- Rosette_768425_MT/app/src/main/java/com/f19/rosette_768425_mt/MainActivity.java
+++ Rosette_768425_MT/app/src/main/java/com/f19/rosette_768425_mt/MainActivity.java
@@ -3,12 +3,40 @@ package com.f19.rosette_768425_mt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Integer numdays = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        final TextView numtxt = findViewById(R.id.numdays);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                numtxt.setText(String.valueOf(i));
+                numdays = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+
     }
 }
